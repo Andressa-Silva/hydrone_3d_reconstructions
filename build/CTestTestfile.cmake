b80eb424108dbed7d9354380ff130deb9737d88a
Andressa-Silva <andressacavalcante94@gmail.com>
2023-10-08 10:08:57 -0300

    add spawn drone

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/andressa/prh_ws2/src
+# Build directory: /home/andressa/prh_ws2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("hydrone_aerial_description")
---
+++ build/hydrone_aerial_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/andressa/prh_ws2/src/hydrone_aerial_description
+# Build directory: /home/andressa/prh_ws2/build/hydrone_aerial_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
